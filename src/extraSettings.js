19755fba13e766384ad8f02e460cd5fb1721a050
ZhihengYi <75360918+ZhihengYi@users.noreply.github.com>
2021-02-01 13:49:34 +0800

    Update extraSettings.js

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -3,10 +3,10 @@
  */
 
 // 额外分数：修改数字
-let extraScore = 1;
+let extraScore = 10000;
 
 // 无敌模式：true 改为 false
-let wuDi = true;
+let wuDi = false;
 
 // 第一个水果：修改数字为 0-10
 let firstFruit = 0;
@@ -22,13 +22,13 @@ let setFruits = {
 }
 
 // 让水果更 Q 弹：false 改为大于 0 小于 1 的任意小数（推荐 0.9）
-let fruitQTan = false;
+let fruitQTan = 0.9;
 
 // 让水果下落缓慢：false 改为大于 0 的任意数，值越大阻力越大，下落越慢（推荐 5）
 let fruitSlowDown = false;
 
 // 广告链接：false 或为空字符串表示不会跳转到广告
-let adLink = 'https://zhihengyi.gitee.io/Synthetic-watermelon/';
+let adLink = 'https://zhihengyi.github.io/Synthetic-watermelon/';
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
 document.getElementsByTagName("title")[0].innerText = '合成大西瓜';
